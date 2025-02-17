21dcf40da48e1eb209926d5fc7ea9cc30a05f466
Varad Wagh <146649167+varadwagh327@users.noreply.github.com>
2025-02-17 16:49:50 +0000

    SOME CHANGES

--- dashboard/src/components/Login.jsx
+++ dashboard/src/components/Login.jsx
@@ -47,7 +47,7 @@ const Login = () => {
     <>
       <section className="container form-component">
         <img src={logo} alt="logo" className="logo" />
-        <h1 className="form-title">WELCOME TO ZEECARE</h1>
+        <h1 className="form-title">WELCOME TO VCARE</h1>
         <p>Only Admins Are Allowed To Access These Resources!</p>
         <form onSubmit={handleLogin}>
           <input
